189420cbd4b2a75734fe077c9370824d5ddd4adc
HyomYom <hyomyang@gmail.com>
2023-06-26 16:51:04 +0900

    update

--- Account/src/main/java/com/example/account/repository/AccountRepository.java
+++ Account/src/main/java/com/example/account/repository/AccountRepository.java
@@ -1,13 +1,23 @@
 package com.example.account.repository;
 
 import com.example.account.domain.Account;
+import com.example.account.domain.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {// 1.사용할 Entity, 2.해당 Entity의 Key Type
 
     Optional<Account> findFirstByOrderByIdDesc();
+
+    Integer countByAccountUser(AccountUser accountUser);
+
+    Optional<Account> findByAccountNumber(String AccountNumber);
+
+    List<Account> findByAccountUser(AccountUser accountUser);
+    // Account에 연관관계로 포함된 AccountUser가 있기 때문에 다음 메소드가 해당 인터페이스
+    // 내에서 자동으로 생성이 됨
 }
