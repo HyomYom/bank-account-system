854b17a17f61b76805bd946448bca6f9e602ff8d
HyomYom <hyomyang@gmail.com>
2023-06-27 10:21:55 +0900

    update

--- Account/src/test/java/com/example/account/controller/AccountControllerTest.java
+++ Account/src/test/java/com/example/account/controller/AccountControllerTest.java
@@ -17,6 +17,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -41,54 +43,54 @@ class AccountControllerTest {
     private ObjectMapper objectMapper; // 객체를 json타입으로 변경 
 
     @Test
-        void successCreateAccount() throws Exception {
-            //given
-        given(accountService.createAccount(anyLong(),anyLong() ))
+    void successCreateAccount() throws Exception {
+        //given
+        given(accountService.createAccount(anyLong(), anyLong()))
                 .willReturn(AccountDto.builder()
                         .userId(1L)
                         .accountNumber("1234567890")
                         .registeredAt(LocalDateTime.now())
                         .unRegisteredAt(LocalDateTime.now())
                         .build());
-            //when
+        //when
         mockMvc.perform(post("/account")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(
-                        new CreateAccount.Request(1L, 100L)
-                ))
-        )
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new CreateAccount.Request(1L, 100L)
+                        ))
+                )
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.userId").value(1))
                 .andExpect(jsonPath("$.accountNumber").value("1234567890"))
                 .andDo(print())
         ;
-            //then
-        }
+        //then
+    }
 
     @Test
-        void successGetAccount() throws Exception {
-            //given
-            given(accountService.getAccount(anyLong()))
-                    .willReturn(Account.builder()
-                            .accountNumber("3456")
-                            .accountStatus(AccountStatus.IN_USE)
-                            .build());
-            //when
+    void successGetAccount() throws Exception {
+        //given
+        given(accountService.getAccount(anyLong()))
+                .willReturn(Account.builder()
+                        .accountNumber("3456")
+                        .accountStatus(AccountStatus.IN_USE)
+                        .build());
+        //when
 
-            //then
-            mockMvc.perform(get("/account/876"))
-                    .andDo(print())
-                    .andExpect(jsonPath("$.accountNumber").value("3456"))
-                    .andExpect(jsonPath("$.accountStatus").value("IN_USE"))
-                    .andExpect(status().isOk());
+        //then
+        mockMvc.perform(get("/account/876"))
+                .andDo(print())
+                .andExpect(jsonPath("$.accountNumber").value("3456"))
+                .andExpect(jsonPath("$.accountStatus").value("IN_USE"))
+                .andExpect(status().isOk());
 
 
-        }
+    }
 
     @Test
     void successDeleteAccount() throws Exception {
         //given
-        given(accountService.deleteAccount(anyLong(),anyString() ))
+        given(accountService.deleteAccount(anyLong(), anyString()))
                 .willReturn(AccountDto.builder()
                         .userId(1L)
                         .accountNumber("1234567890")
@@ -108,6 +110,37 @@ class AccountControllerTest {
                 .andDo(print())
         ;
         //then
+    }
+
+    @Test
+    void successGetAccountsByUserId() throws Exception {
+        //given
+
+        List<AccountDto> accountDtos =
+                Arrays.asList(
+                        AccountDto.builder()
+                                .accountNumber("1234567890")
+                                .balance(1000L).build(),
+                        AccountDto.builder()
+                                .accountNumber("3456789012")
+                                .balance(1000L).build(),
+                        AccountDto.builder()
+                                .accountNumber("6789012345")
+                                .balance(1000L).build()
+                );
+        given(accountService.getAccountsByUserId(anyLong()))
+                .willReturn(accountDtos);
+        //when
+        //then
+
+        mockMvc.perform(get("/account?user_id=1"))
+                .andDo(print())
+                .andExpect(jsonPath("$[0].accountNumber").value("1234567890"))
+                .andExpect(jsonPath("$[0].balance").value(1000))
+                .andExpect(jsonPath("$[1].accountNumber").value("3456789012"))
+                .andExpect(jsonPath("$[1].balance").value(1000))
+                .andExpect(jsonPath("$[2].accountNumber").value("6789012345"))
+                .andExpect(jsonPath("$[2].balance").value(1000));
     }
 
 }
--- Account/src/test/java/com/example/account/service/AccountServiceTest.java
+++ Account/src/test/java/com/example/account/service/AccountServiceTest.java
@@ -17,7 +17,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -243,6 +246,7 @@ class AccountServiceTest {
         //then
         assertEquals(ErrorCode.ACCOUNT_NOT_EMPTY, exception.getErrorCode());
     }
+
     @Test
     @DisplayName("해지된 계좌는 해지할 수 없다.")
     void deleteAccountFailed_alreadyUnregistered() {
@@ -256,7 +260,7 @@ class AccountServiceTest {
         given(accountRepository.findByAccountNumber(anyString()))
                 .willReturn(Optional.of(Account.builder()
                         .accountUser(pobi)
-                                .accountStatus(AccountStatus.UNREGISTERED)
+                        .accountStatus(AccountStatus.UNREGISTERED)
                         .balance(0L)
                         .accountNumber("1000000012")
                         .build()));
@@ -266,6 +270,61 @@ class AccountServiceTest {
         //then
         assertEquals(ErrorCode.ACCOUNT_ALREADY_UNREGISTERED, exception.getErrorCode());
     }
+
+    @Test
+    void successGetAccountsByUserId() {
+        //given
+        AccountUser pobi = AccountUser.builder()
+                .id(12L)
+                .name("Pobi")
+                .build();
+        List<Account> accounts = Arrays.asList(
+                Account.builder()
+                        .accountUser(pobi)
+                        .accountNumber("1111111111")
+                        .balance(1000L)
+                        .build(),
+                Account.builder()
+                        .accountUser(pobi)
+                        .accountNumber("2222222222")
+                        .balance(2000L)
+                        .build(),
+                Account.builder()
+                        .accountUser(pobi)
+                        .accountNumber("3333333333")
+                        .balance(3000L)
+                        .build()
+        );
+
+        given(accountUserRepository.findById(anyLong()))
+                .willReturn(Optional.of(pobi));
+        given(accountRepository.findByAccountUser(any()))
+                .willReturn(accounts);
+        //when
+        List<AccountDto> accountDtos = accountService.getAccountsByUserId(1L);
+
+        //then
+        assertEquals(3, accountDtos.size());
+        assertEquals("1111111111", accountDtos.get(0).getAccountNumber());
+        assertEquals(1000,accountDtos.get(0).getBalance());
+        assertEquals("2222222222", accountDtos.get(1).getAccountNumber());
+        assertEquals(2000,accountDtos.get(1).getBalance());
+        assertEquals("3333333333", accountDtos.get(2).getAccountNumber());
+        assertEquals(3000,accountDtos.get(2).getBalance());
+    }
+
+    @Test
+        void failedToGetAccounts() {
+            //given
+            given(accountUserRepository.findById(anyLong()))
+                    .willReturn(Optional.empty());
+
+            //when
+        AccountException exception = assertThrows(AccountException.class, () ->
+                accountService.getAccountsByUserId(1L));
+            //then
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+        }
 
 //    @Test
 //    @DisplayName("계좌 조회 성공")
